f7e8ab87b209094be123b37a4b0d1e66e2ffebd1
Arafat-Alsaieq <a.h.b.alsaieq@gmail.com>
2024-10-23 10:44:35 +0300

    Initial Commit

---
+++ WaterTrackerApp/WaterTrackerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaterTrackerAppApp.swift
+//  WaterTrackerApp
+//
+//  Created by Arafat on 23/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct WaterTrackerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WaterTrackerAppTests/WaterTrackerAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  WaterTrackerAppTests.swift
+//  WaterTrackerAppTests
+//
+//  Created by Arafat on 23/10/2024.
+//
+
+import Testing
+@testable import WaterTrackerApp
+
+struct WaterTrackerAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
